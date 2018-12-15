ecd50e0891f811f4d9567e1908f92c89164fe610
Risabh Mishra <risabhmishra19@gmail.com>
2018-12-15 18:41:20 +0530

    Server

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/demo")
-def demo():
-    return render_template("index2.html")
 
-@app.route("/demofinal", methods=['POST'])
-def demofinal():
-    text = request.form['api_url']
-	
-    
-    return render_template("index3.html", label)
 
 
 if __name__ == "__main__":
